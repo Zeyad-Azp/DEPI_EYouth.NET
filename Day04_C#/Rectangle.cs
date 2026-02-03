1d460ea10590a0da29fef98cba44ac1509f34539
zeyadazp <162899090+Zeyad-Azp@users.noreply.github.com>
2026-02-03 23:07:57 +0200

    Add files via upload

---
+++ Day04_C#/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEPI_Day02
+{
+    internal class Rectangle
+    {
+        public int leangth;
+        public int width;
+    }
+}
